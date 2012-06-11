aa505b45db41c59eff9828dadf477b26e3462743
Jon Roundy <jonroundy@fullsail.edu>
2012-06-11 00:05:35 -0500

    Completion of the project
    Not my best work :?

--- roundy_jon_assignment2.js
+++ roundy_jon_assignment2.js
@@ -1,96 +1,85 @@
-//alert("JavaScript works!");
+//Jon Roundy
+//SDI Project 2
+//Term 0612 Week 2
 
 var teamName;
-var teamCount = 5;
+var towersStanding = 6;
+var towersDestroyed = 0;
 var turretCount = ["Top Lane 1", "Top Lane 2", "Mid Lane 1", "Mid Lane 2", "Bottom Lane 1", "Bottom Lane 2"];
 var readyToPlay = true;
-
 var rushBase = "towers are down charge the base!";
 var atkNexus = "we are in attack the nexus!";
-var turretsAllDown = false;
-
+var turretsAllDown = true;
 var notReady = false;
 var otherTeamReady = true;
-
-
-function letsPlay(weekEnd){
-
-	if (weekEnd === true) {
-		return console.log("Yes I have time to play.");
-	} else {
-		return console.log("No I do not have time to play");
-	}
-
-};
-letsPlay(weekEnd);
-
-function startMatch(){
-	var notReady = false;
-	var otherTeamReady = true;
-	
-	if ((otherTeamReady && readyToPlay) || notReady) {
-		console.log("I will gather my team and start the match.");
-	} else {
-		console.log("We need all members to start the match.");
-		
-	};
-	
-	
-}; // Boolean function
-startMatch();
-
-var towerTakeDown = function(){
-	for(var i = 0; i < 6; i++){
-		var towerNames = [
+var weekEnd = true;
+var towerNames = [
 			"Top 1",
 			"Top 2",
 			"Middle 1",
 			"Middle 2",
 			"Bottom 1",
-			"Bottom 2",
+			"Bottom 2"
 	];
-	
-	console.log("Once match is started we need to take out towers " + towerNames[i] )
-	}
-	return  i
+var weWon = "Look at that we won the match!";
 
-
-};
-
-
-towerTakeDown();
-
-var lanesAttacked = function(){
-
-	var towersStanding = 6;
-	while (towersStanding > 0) {
-	console.log(towersStanding + " Towers need to be destroyed before we can rush base!");
-	towersStanding--;
-	};
-		
-	}; // End of number function
-lanesAttacked();
-
-myFunction('Harry Potter' , 'Wizard'){
-function myFunction(name,job)
-console.log("welcone" + name + ", the " job);
-}
-	
-
-}; //end of string function
-
-/*
-var rushBase = "towers are down charge the base!";
-var atkNexus = "we are in attack the nexus!";
-var turretsAllDown = false;
-
-var letsAttack = function(){
-	var test 1
-
+var letsAttack = function () {
 	if(turretsAllDown === true) {
-		 console.log("Everyone the " + rushBase + " Ok everyone " + atkNexus );
+		console.log("Everyone the " + rushBase + " Ok everyone " + atkNexus);
 	} else {
 		console.log("there is nothing left we can do");
 	};
-*/
+}; //String function
 
+var lanesAttacked = function (towersStanding) {
+var totalTowers = 6;
+var percentageDestroyed;
+	while (towersStanding > 0) {
+	percentageDestroyed = towersStanding/totalTowers;
+	if (percentageDestroyed !== 1) {
+	} else {
+	console.log("100% of the towers have been destroyed.");
+	return letsAttack(); 
+	}
+	towersStanding--;
+	} 
+		
+}; // End of number function
+
+var towerTakeDown = function (towerNames,towersDestroyed) {
+	for(var i = towersDestroyed ; i < towersStanding; i++){
+	console.log("Once match is started we need to take out towers " + towerNames[i] )
+	}
+	return lanesAttacked(i);
+
+}; //end of array
+
+var startMatch = function (readyToPlay,notReady,otherTeamReady) {
+	if ((otherTeamReady && readyToPlay) || notReady) {
+	console.log("I will gather my team and start the match.");
+		return towerTakeDown(towerNames,towersDestroyed);
+	} else {
+		console.log("We need all members to start the match.");
+		return false;
+		
+	};
+	
+}; // Boolean function
+
+var letsPlay = function (weekEnd) {
+
+	if (weekEnd === true) {
+		console.log("Yes I have time to play.");
+		return startMatch(readyToPlay,notReady,otherTeamReady);
+	} else {
+		console.log("No I do not have time to play");
+		return false;
+	}
+
+}; //Procedure 
+letsPlay(weekEnd);
+
+var finish; 
+	if (weekEnd === true) {
+		console.log(weWon);
+	}
