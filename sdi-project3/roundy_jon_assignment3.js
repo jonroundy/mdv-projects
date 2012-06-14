796dff658f4bb9c283b833991499832ad170a7f9
Jon Roundy <jonroundy@fullsail.edu>
2012-06-14 11:47:56 -0500

    Update to assignment3.js
    Added function with boolean argument along with conditional

--- sdi-project3/roundy_jon_assignment3.js
+++ sdi-project3/roundy_jon_assignment3.js
@@ -4,7 +4,35 @@
 // In the situtation of needing to use a value of 0 as the begining value javascript will not see it use a string in these situations
 // While using an object I can also use another array of data within it. Allowing for more complex code that rests within another complex data type
 
-var readyToPlay = true
+//Global Variables
+
+var membersGathered = true
+var readyToPlay = true;
+
+var gatherMembers = function (membersGathered) {
+
+	if (weekEnd === true) {
+		console.log("Yes I have time to play.");
+		return startMatch(readyToPlay,notReady,otherTeamReady);
+	} else {
+		console.log("No I do not have time to play");
+		return false;
+	}
+
+}; //Procedure
+
+
+var findMatch = function (readyToPlay) {
+	
+	if (readyToPlay === true){
+		console.log("Since we are ready to play lets find a match.");
+	} else {
+		console.log("Who are we missing?");
+		return false;
+	}
+};
+
+findMatch(readyToPlay);
 
 var team = { 	name: "Viral", 
 				members:[
@@ -27,8 +55,8 @@ var team = { 	name: "Viral",
 	},
 }; // End of object
 
-var key = "heros";
 
+var key = "heros";
 for (var key in team.heros ) {
 	console.log("Home Roster: " + "~ Player Name: = " + key + ", Hero: " + team.heros[key]);
 	
@@ -37,8 +65,6 @@ for (var key in team.heros ) {
 
 var key = "members";
 
-team.sayHi();
-
 var handleData = function (opponent) {
 	for (var i = 0; i < opponent.members.length; i++){
 		var member = opponent.members[i];
@@ -46,14 +72,25 @@ var handleData = function (opponent) {
 	};
 };
 
+var didWeWin 
+
 handleData(opponent2);
 
+team.sayHi();
 
-var jsonstring = JSON.stringify(opponent);
-console.log(jsonstring);
+var matchResults = function ()
 
-var receivedjson = JSON.parse(jsonstring);
-console.log(receivedjson);
+//team["sayHi"]();
+
+//team.sayHi = function () {
+//		console.log("Get lost."); I need to use this mutator
+//};
+
+/*var jsonstring = JSON.stringify(opponent);
+console.log(jsonstring); */
+
+/* var receivedjson = JSON.parse(jsonstring);
+console.log(receivedjson); */
 
 
 //Gathered Notes and variations of code - REMOVE ALL BEFORE FINAL COMMIT AND TURN IN
