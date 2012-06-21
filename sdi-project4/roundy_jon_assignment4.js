44d8861b822f6fc4bdbc39392a3c4d2a600f4b72
Jon Roundy <jonroundy@fullsail.edu>
2012-06-21 11:46:39 -0500

    Made changes to library.
    Added Library
    Completed Problem 1, 123-456-7890 String
    Completed Problem 2, aaa@bbb.ccc String

--- sdi-project4/roundy_jon_assignment4.js
+++ sdi-project4/roundy_jon_assignment4.js
@@ -2,27 +2,55 @@
 //SDI Project 3 1206 Week 4
 
 //This is my project 4 library.
+var library = function (){
+
 //Scenario 1 Does a string follow a 123-456-7890 pattern like a phone number?
+//The function uses a pattern of a series of 10 numbers formated in 3 sections two sets of 3 digits and one set of 4.
+// Problem 1 starts here
 
-
-var myLibrary = {
-	function usNumValidation(inputedNumber) {
+ var usNumValidation = function (inputedNum) {
 	
-	var usNumPat = /^\(?([0-9]{3})\)?[-. ]?([0-9{3}])[-. ]?([0-9]{4})$/; //Regular Expression 
-
-	if (usNumValidation.test(inputedNu)) { // Determines if inputed value matched pattern. 
-			var inputedNumber = "";
-			var validatedPhoneNumber = inputedNumber.replace(numberValidation, "($1) $2-$3");
-			console.log("Phone number validated.")
+	var value = "inputedNum";
+	var	usNumPat = /^\(?([0-9]{3})\)?[\-. ]?([0-9]{3})[\-. ]?([0-9]{4})$/;
+	// Above - Regular Expression, a pattern is used to check if the supplied values match the criteria
+		if (usNumPat.test(inputedNum)) {  // Determines if value is within pattern rules
+				console.log("Phone number validated."); // Sends string into console log
 		} else {
-			console.log("Not a Valid Phone Number!") // Outputs string into console log
-			alert("Please enter a valid phone number!") // For flavor please see index page of project to see use.
+				console.log("Not a valid Phone Number!"); // Sends string into console log
 			return this;
-	} 
-
 	}
 
-};
-//alert(myLibrary.usNumValidation);
+	}; // End of Problem 1.
 
-usNumValadation("912-342-7398");
+// Problem 2 Starts here
+
+ var emailValidation = function (emailInfo) {
+	
+	var emailAddress = "emailInfo";
+	var	emailPat = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,4}$/; //Regular Expression
+		if (emailPat.test(emailInfo)) {  // Determines if value is within pattern rules
+				console.log("Email address validated."); // Sends string into console log
+		} else {
+				console.log("Not a valid email address!"); // Sends string into console log
+			return this;
+	}
+
+	}; // End of Problem 1.
+
+return{
+	"usNumValidation" : usNumValidation,
+	"emailValidation" : emailValidation
+
+	};
+	// End of function returns
+};
+// End of library
+
+// Function Calls
+// Problem 1
+
+var lib = library();
+lib.usNumValidation("912-342-7398");
+lib.emailValidation("test@abc.com");
+
+
