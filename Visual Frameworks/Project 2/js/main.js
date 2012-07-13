3f8523476640d28e405bf32a685bff6b77c61ae2
Jon Roundy <jonroundy@fullsail.edu>
2012-07-13 00:59:31 -0500

    Save Data now fully working!
    YAY now on to display.

--- Visual Frameworks/Project 2/js/main.js
+++ Visual Frameworks/Project 2/js/main.js
@@ -28,32 +28,53 @@ window.addEventListener("DOMContentLoaded", function(){
 			selectLi.appendChild(makeSelect);
 	}
 	
+	function getCheckboxVault(){
+		if(g('urgent').checked){
+			urgentValue = g('urgent').value;
+		}else{
+			urgentValue	= "No"
+		}
+	}
+	
 	//Save data into local storage.
 	function storeData(key){
-		//Find Values of Checkboxes and Radios
-		//setCheckboxValue();
-		//Save form elements into LS
-		localStorage.setItem("Category List", g('groups').value);
-		localStorage.setItem("Task Name", g('taskname'.value);
-		alert("Task Saved!");
-	}
+			var id = Math.floor(Math.random()*100000001);
+			//Gather up all our form field values and store in an object.
+			//Object properties are going to contain array with the form label and input value
+			getCheckboxVault();
+			var item 				= {};
+			item.cats 				= ["Category List: ", g("groups").value];
+			item.taskname 			= ["My Task Name: ", g("taskname").value];
+			item.date 				= ["Date: ", g("date").value];
+			item.time 				= ["Time: ", g("time").value];
+			item.urgent 			= ["Urgent: ", urgentValue];
+			item.slider1			= ["Estimated Time.", g("slider1").value];
+			item.textbox			= ["Notes: ", g("textbox").value];
+			//Save data into Local Storage: Use Stringify to convert our object to a 	string. Local storage only stores strings.
+			//Save form elements into LS
+			localStorage.setItem(id, JSON.stringify(item));
+			alert("Task Saved!");
+}
 	
 	
 	//Clear all data
-	//function clearLocal() {
-//		if(localStorage.lengh === 0){
-	//		alert("There is no data to clear.");
-	//		}else{
-	//			localStorage.clear();
-	//			alert("All tasks deleted.");
-	//			window.location.reload();
-	//			return false;
-	//	}
+	function clearLocal() {
+		if(localStorage.lengh === 0){
+			alert("There is no data to clear.");
+		}else{
+			localStorage.clear();
+			alert("All tasks deleted.");
+			window.location.reload();
+			return false;
+		}
 		
-	//}
+	}
+	
 	
 	//Variable defaults
-	var categoryLists = ["--Choose A Category--", "Personal", "Work", "Misc"];
+	var categoryLists = ["--Choose A Category--", "Personal", "Work", "Misc"],
+		urgentValue = "No"
+	;
 	makeCats();
 	
 	
